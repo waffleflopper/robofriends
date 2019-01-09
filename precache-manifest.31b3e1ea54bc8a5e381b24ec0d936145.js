cb63628c11accf70994d435f9fd0bce2cc599cc4
Robert <robertbaddeley@gmail.com>
2019-01-09 04:24:18 -1000

    Updates

---
+++ precache-manifest.31b3e1ea54bc8a5e381b24ec0d936145.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2183febec155ef02a0b1",
+    "url": "/robofriends/build/static/css/main.2405acd5.chunk.css"
+  },
+  {
+    "revision": "2183febec155ef02a0b1",
+    "url": "/robofriends/build/static/js/main.2183febe.chunk.js"
+  },
+  {
+    "revision": "5459e8e5d5a6db2f42f0",
+    "url": "/robofriends/build/static/js/1.5459e8e5.chunk.js"
+  },
+  {
+    "revision": "9fff56afaa35cfb83a00",
+    "url": "/robofriends/build/static/js/runtime~main.9fff56af.js"
+  },
+  {
+    "revision": "d38fcc089d31233fa46fac70e59ab5fa",
+    "url": "/robofriends/build/index.html"
+  }
+];
